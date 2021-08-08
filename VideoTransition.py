754a072dcf8cc79b740a2a1e9d18d8ce0c604b9b
Rafael Souza Oliveira <rafael_gravity@hotmail.com>
2021-08-07 23:05:29 -0300

    slide transition prototype

--- VideoTransition.py
+++ VideoTransition.py
@@ -3,26 +3,25 @@ import numpy as np
 
 # Create a VideoCapture object and read from input file
 # If the input is the camera, pass 0 instead of the video file name
-cap = cv2.VideoCapture('videos\Bees.mp4')
+cap1 = cv2.VideoCapture('videos/Sand.mp4')
+cap2 = cv2.VideoCapture('videos/Bees.mp4')
 
 # Check if camera opened successfully
-if (cap.isOpened() == False): 
+if (cap1.isOpened() == False): 
   print("Error opening video stream or file")
 
-#height, width = cap.shape[:2]
-vel = 0.05
-#width  = cap.get(3)  # float `width`
-#height = cap.get(4)  # float `height`
+i = 2
+vel = 0.02
 
-#print('width, height:', width, height)
-i = 1
-vel = 0.01
 # Read until video is completed
-while(cap.isOpened()):
+while(cap1.isOpened()):
   # Capture frame-by-frame
-  ret, frame = cap.read()
-  frame = cv2.resize(frame, (540, 380), fx = 0, fy = 0, interpolation = cv2.INTER_CUBIC)
-  height, width = frame.shape[:2]
+  ret, frame1 = cap1.read()
+  frame1 = cv2.resize(frame1, (540, 380), fx = 0, fy = 0, interpolation = cv2.INTER_CUBIC)
+  ret, frame2 = cap2.read()
+  frame2 = cv2.resize(frame2, (540, 380), fx = 0, fy = 0, interpolation = cv2.INTER_CUBIC)
+
+  height, width = frame1.shape[:2]
 
   if ret == True:
     # Display the resulting frame
@@ -32,11 +31,12 @@ while(cap.isOpened()):
   
     # We use warpAffine to transform
     # the image using the matrix, T
-    img_translation = cv2.warpAffine(frame, T, (width, height))
-  
+    mashup = np.concatenate((frame2, frame1), axis=1)
+
+    img_translation = cv2.warpAffine(mashup, T, (width, height))
     cv2.imshow('Translation', img_translation)
     cv2.waitKey(25)  
-    
+
     i = i - vel
 
     if cv2.waitKey(25) & 0xFF == ord('q'):
@@ -46,9 +46,8 @@ while(cap.isOpened()):
   else: 
     break
 
-
 # When everything done, release the video capture object
-cap.release()
+cap1.release()
 
 # Closes all the frames
 cv2.destroyAllWindows()
